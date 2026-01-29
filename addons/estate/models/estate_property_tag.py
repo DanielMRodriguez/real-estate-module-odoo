f1c03593252a87c62760f1b8ea5c7001949a1948
Luis Mendoza <desarrollo@dxt.com.mx>
2026-01-29 16:56:29 -0600

    attribute color to model tag

--- addons/estate/models/estate_property_tag.py
+++ addons/estate/models/estate_property_tag.py
@@ -6,7 +6,7 @@ class EstatePropertyTag(models.Model):
     _order = "name DESC"
 
     name = fields.Char(string="Tag Name", required=True)
-    
+    color = fields.Integer()
     _sql_constraints = [
         ('check_name', 'UNIQUE(name)', 'The tag name must be unique'),
     ]
